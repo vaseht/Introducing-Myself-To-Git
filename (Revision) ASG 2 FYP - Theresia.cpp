4df250cf7b6d76c2fc33f1ba87a4272852354e18
vaseht <theresia.budiharto@binus.ac.id>
2025-09-02 22:41:08 +0700

    (Revision) ASG 2 FYP - Theresia.cpp

---
+++ (Revision) ASG 2 FYP - Theresia.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello! My name is Theresia Angelica,\n");
+    printf("and my favorite programming language is Python and Cpp.");
+
+    return 0;
+}
