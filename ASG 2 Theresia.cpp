fa96a0e2170a28cfc9d867d6e034d54785b97bb8
Vaseht <theresia.budiharto@binus.ac.id>
2025-09-02 22:18:41 +0700

    first commit

---
+++ ASG 2 Theresia.cpp
@@ -0,0 +1,15 @@
+/******************************************************************************
+
+hello hello hello
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello! My name is Theresia Angelica,\n");
+    printf("and my favorite programming language is Python and C.");
+
+    return 0;
+}
